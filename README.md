a82cae1a3c1d0a6a12bb333f1491577af3fd0db2
Jan Biernacik <jan@biernacik.dev>
2025-06-05 04:20:25 +0200

    This needs to be fixed

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # rectify-gnome
+
+##  NOTICE: The part of the script responsible for installing the meta package was broken, so I’ve removed it for now. As a result, the script is currently non-functional.
+
+## Prerequisites
+- git
+
 ## Installation
+
 Run as root:
 ```
 git clone https://github.com/janjbnck/rectify-gnome.git
+cd rectify-gnome/
 chmod +x install.sh
 bash install.sh
 ```
--- install.sh
+++ install.sh
@@ -8,10 +8,9 @@ fi
 apt update && DEBIAN_FRONTEND=noninteractive apt upgrade -y
 DEBIAN_FRONTEND=noninteractive apt install gnupg curl -y
 (cd /tmp && curl -s https://julianfairfax.codeberg.page/package-repo/pub.gpg | gpg --dearmor | dd of=/usr/share/keyrings/julians-package-repo.gpg && echo 'deb [ signed-by=/usr/share/keyrings/julians-package-repo.gpg ] https://julianfairfax.codeberg.page/package-repo/debs packages main' | tee /etc/apt/sources.list.d/julians-package-repo.list)
-dpkg -b ./rectify-gnome-meta
-
 apt update
-dpkg -i ./rectify-gnome-meta.deb
+
+# The part of the script responsible for installing the meta package was broken, so I’ve removed it for now. As a result, the script is currently non-functional.
 
 laptop-detect && {
   DEBIAN_FRONTEND=noninteractive apt install tlp tlp-rdw -y
